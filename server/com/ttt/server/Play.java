c8ec2b76a8a71fdbfaf6aa4ce58601df522134b8
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-01-16 01:00:43 +0100

    Place symbols in the board

--- server/com/ttt/server/Play.java
+++ server/com/ttt/server/Play.java
@@ -1,11 +1,36 @@
 package com.ttt.server;
 
+import java.util.Scanner;
 
 public class Play {
 	
+	public static char [][] board = {{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
+	public static char symbol = 'X';
+	
 	public static void main(String[] args){	
 		
-		char [][] board = {{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the coordinates of your symbol: ");
+		String Coordinates = scanner.next();
+		
+		int x = Character.getNumericValue(Coordinates.charAt(0));
+		int y = Character.getNumericValue(Coordinates.charAt(1));
+		System.out.println(y);
+		placeSymbol(x, y, symbol);
+		displayBoard();
+	}
+	
+	public static boolean checkPositionValidity(int x, int y) {
+		return (x<3 && y<3 && 	board[x][y] == ' ');
+	}
+	
+	
+	public static void placeSymbol( int x, int y, char symbol) {
+		System.out.println(x);
+		board[x][y] = symbol;
+	}
+	
+	public static void displayBoard() {
 		System.out.printf("+-----------------------------+\n");
 
 	    for (int i = 0; i < 3; i++)
@@ -13,5 +38,6 @@ public class Play {
 	        	System.out.printf("%c%5c%5c%5c%5c%5c%5c\n", '|', board[i][0], '|', board[i][1], '|', board[i][2], '|');
 	            System.out.printf("+-----------------------------+\n");
 	        }
+	    
 	}
 }
